3f594deb1416be381f3d0ee367e73d31381f6103
Naman Malhotra <71208368+naman-mh@users.noreply.github.com>
2024-08-29 16:53:37 +0000

    SignUp page added

--- src/components/register/SignUp.js
+++ src/components/register/SignUp.js
@@ -1,50 +1,81 @@
-import { Box, Input,InputAdornment ,TextField } from "@mui/material";
+import { Box, Button, Input, InputAdornment, Typography } from "@mui/material";
 import React from "react";
-import { Search } from '@mui/icons-material'
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import HttpsIcon from '@mui/icons-material/Https';
+import PhoneEnabledIcon from '@mui/icons-material/PhoneEnabled';
+import EmailIcon from '@mui/icons-material/Email';
 function SignUp() {
   return (
-    <Box bgcolor="#ADD8E6" height={"93vh"} display={"flex"} alignItems={"center"} justifyContent={"center"} >
-      <Box 
+    <Box bgcolor="#faebd7" height={"93vh"}  >
+
+      <Box
         sx={{
-          border: "5px solid red",
-          height:540,
-          color: "blue",
-          
-          width:{xs:"360px",sm:"450px",md:"550px", lg:"600px"},
+
+          height: 540,
+          borderRadius: "15px",
+          width: { xs: "360px", sm: "450px", md: "550px", lg: "600px" },
           display: "flex",
+          flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
-        }}
-      >
-        
-        <Box display={"flex"} flexDirection={"column"}>
-        {/* <div style={{
-         display:"flex",
-         justifyContent:"space-between"
-        }}> */}
-        
-        <Input
-          id="input-with-icon-adornment"
-          sx={{border:"2px solid black",outline:"none" }}
-          startAdornment={
-            <InputAdornment position="start">
-              <AccountCircleIcon />
-            </InputAdornment>
-          }
-        />
-        
-        <Input  type="text" placeholder="Username" bgcolor="red"></Input>
-        <Input type="number" placeholder="Phone Number"></Input>
-        <Input type="email" placeholder="Email"></Input>
-        <Input type="password" placeholder="Password"></Input>
-        <Input type="password" placeholder="Confirm Password"></Input>
+          bgcolor: "#e3d4c1",
+          margin: "0 auto",
+          textAlign:"left"
+        }}>
+        <Typography variant="h5" >Register</Typography>
+
+        <Box display={"flex"} flexDirection={"column"} sx={{ width: { xs: "360px", sm: "450px", md: "550px" }, }} >
+          <Input
+            type="text" placeholder="Username"
+            id="input-with-icon-adornment"
+            sx={{ outline: "none", margin: "10px" }}
+            startAdornment={
+              <InputAdornment position="start">
+                <AccountCircleIcon />
+              </InputAdornment>
+            }
+          />
+          <Input
+            type="number" placeholder="Phone Number"
+            id="input-with-icon-adornment"
+            sx={{ outline: "none", margin: "10px" }}
+            startAdornment={
+              <InputAdornment position="start">
+                <PhoneEnabledIcon />
+              </InputAdornment>
+            }
+          />
+          <Input
+            type="email" placeholder="Email"
+            id="input-with-icon-adornment"
+            sx={{ outline: "none", margin: "10px" }}
+            startAdornment={
+              <InputAdornment position="start">
+                <EmailIcon />
+              </InputAdornment>
+            }
+          />
+          <Input
+            type="Password" placeholder="Password"
+            id="input-with-icon-adornment"
+            sx={{ outline: "none", margin: "10px" }}
+            startAdornment={
+              <InputAdornment position="start">
+                <HttpsIcon />
+              </InputAdornment>
+            }
+          />
+          <Input
+            type="Password" placeholder="Confirm Password"
+            id="input-with-icon-adornment"
+            sx={{ outline: "none", margin: "10px" }}
+          />
+
         </Box>
-        
-        
-        
+        <Button sx={{ margin: "10px", bgcolor: "#228b22", color: "white" , padding:"auto" ,width:"100px"  }}>Submit</Button>
       </Box>
     </Box>
+
   );
 }
 
