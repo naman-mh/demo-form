0b29a9c4d684041b3313483deca6cd9e8009cae3
Naman Malhotra <namanmalhotra9759@gmail.com>
2024-08-29 07:05:22 +0530

    upload

---
+++ src/components/register/NavComp.js
@@ -0,0 +1,27 @@
+import {  Box, Button, Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NavComp() {
+  return (
+    <Box
+      sx={{
+        height: "7vh",
+        position: "sticky",
+        display: "flex",
+        alignItems: "center",
+        bgcolor: "#334a52",
+        justifyContent: "space-between",
+        padding:"30px"
+      }}
+    >
+      <Typography color="white">Logo</Typography>
+      <Box sx={{ display: "flex" }}>
+        <Button LinkComponent={Link} to="/login" sx={{ color: "white" }}>Login</Button>
+        <Button LinkComponent={Link} to="/signup" sx={{ color: "white" }}>SignIn</Button>
+      </Box>
+    </Box>
+  );
+}
+
+export default NavComp;
---
+++ src/components/register/SignUp.js
@@ -0,0 +1,36 @@
+import { Box, Input } from "@mui/material";
+import React from "react";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+function SignUp() {
+  return (
+    <Box bgcolor="#ADD8E6" height={"93vh"} display={"flex"} alignItems={"center"} justifyContent={"center"} >
+      <Box 
+        sx={{
+          border: "5px solid red",
+          height:540,
+          color: "blue",
+          
+          width:{xs:"360px",sm:"450px",md:"550px", lg:"600px"},
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        
+        <Box display={"flex"} flexDirection={"column"}>
+        <AccountCircleIcon />
+        <Input  type="text" placeholder="Username" bgcolor="red"></Input>
+        <Input type="number" placeholder="Phone Number"></Input>
+        <Input type="email" placeholder="Email"></Input>
+        <Input type="password" placeholder="Password"></Input>
+        <Input type="password" placeholder="Confirm Password"></Input>
+        </Box>
+        
+        
+        
+      </Box>
+    </Box>
+  );
+}
+
+export default SignUp;
