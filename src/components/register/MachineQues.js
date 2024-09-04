50727a69e8986d1ebece00fa185117981499d801
Naman Malhotra <71208368+naman-mh@users.noreply.github.com>
2024-09-04 23:26:22 +0000

    functionality added

--- src/components/register/MachineQues.js
+++ src/components/register/MachineQues.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState , useEffect } from "react";
 import { Box, Grid, TextField, Typography } from "@mui/material";
 
 
 function MachineQues() {
+  const [inputValue,setInputValue]= useState("")
+  const [inputdescription,setInputDescription]= useState("")
+  const [inputHint,setInputHint]= useState("")
+
+  useEffect(()=>{
+    console.log(inputValue)
+  },[inputValue])
+  
   return (
     <Box sx={{textAlign:"center",bgcolor:"#faebd7",height: "93vh", }}>
        <Typography variant="h4" bgcolor="#faebd7" py={"20px"}>Machine Coding Question</Typography> 
@@ -10,31 +18,30 @@ function MachineQues() {
     <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center",  bgcolor:"#faebd7" }}>
     
       <Grid container >
-        <Grid item xs={12} md={4} >
+        <Grid item xs={12} md={4}  >
           <item>
             <Box
               
               marginTop={{ xs: "90px", md: "auto" }}
               sx={{
-                bgcolor:"red",
                 
+                margin:"0 auto",
                 height: "340px",
                 display: "flex",
                 flexDirection: "column", alignItems: "center", justifyContent: "center"
               }} >
-              <TextField  placeholder="Title" sx={{ width: { xs: "300px", md: "400px" }, mt: "16px" ,
-                 "& .MuiOutlinedInput-notchedOutline": {
-                  borderColor:"green",
-                } }} >Title</TextField>
-              <TextField  placeholder="Description"sx={{ width: { xs: "300px", md: "400px" }, mt: "16px" ,  }} >Description</TextField>
-              <TextField  placeholder="Hint"sx={{ width: { xs: "300px", md: "400px" }, mt: "16px" ,  }} >Hint</TextField>
+              <TextField value={inputValue} onChange={(e)=>setInputValue(e.target.value)} placeholder="Title" sx={{ width: { xs: "280px", md: "300px" , lg:"400px" }, mt: "16px" 
+                 
+                 }} >Title</TextField>
+              <TextField  placeholder="Description"sx={{ width: { xs: "280px", md: "300px" , lg:"400px" }, mt: "16px" ,  }} >Description</TextField>
+              <TextField  placeholder="Hint"sx={{ width: { xs: "280px", md: "300px" , lg:"400px" }, mt: "16px" ,  }} >Hint</TextField>
             </Box></item>
         </Grid>
         <Grid item xs={12} md={8} >
           <item> 
             <Box  
             sx={{ 
-              bgcolor:"pink",
+              
               height: "440px",
               margin: "auto",
               display: "flex",
@@ -50,7 +57,7 @@ function MachineQues() {
 
 
               sx={{
-                margin:'0 10px',
+                margin:'0 30px',
                 height: "340px",
                 width:  { xs: "260px", md: "320px" },
                 flexGrow: 1,
