0bb3d02e56e7760746d3139e469eb9b525922595
Naman Malhotra <71208368+naman-mh@users.noreply.github.com>
2024-09-04 16:29:35 +0000

    error has been removed

--- src/components/register/MachineQues.js
+++ src/components/register/MachineQues.js
@@ -1,65 +1,86 @@
 import React from "react";
-import { Box, Grid, TextField } from "@mui/material";
+import { Box, Grid, TextField, Typography } from "@mui/material";
 
 
 function MachineQues() {
   return (
-    <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", height: "93vh" }}>
-      <Grid container  >
-        <Grid item xs={12} md={6} >
+    <Box sx={{textAlign:"center",bgcolor:"#faebd7",height: "93vh", }}>
+       <Typography variant="h4" bgcolor="#faebd7" py={"20px"}>Machine Coding Question</Typography> 
+    
+    <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center",  bgcolor:"#faebd7" }}>
+    
+      <Grid container >
+        <Grid item xs={12} md={4} >
           <item>
             <Box
-              bgcolor={"grey"}
+              
               marginTop={{ xs: "90px", md: "auto" }}
               sx={{
+                bgcolor:"red",
+                
                 height: "340px",
                 display: "flex",
                 flexDirection: "column", alignItems: "center", justifyContent: "center"
               }} >
-              <TextField sx={{ width: { xs: "300px", md: "400px" }, mt: "16px" }} >Title</TextField>
-              <TextField sx={{ width: { xs: "300px", md: "400px" }, mt: "16px" }} >Description</TextField>
-              <TextField sx={{ width: { xs: "300px", md: "400px" }, mt: "16px" }} >Hint</TextField>
+              <TextField  placeholder="Title" sx={{ width: { xs: "300px", md: "400px" }, mt: "16px" ,
+                 "& .MuiOutlinedInput-notchedOutline": {
+                  borderColor:"green",
+                } }} >Title</TextField>
+              <TextField  placeholder="Description"sx={{ width: { xs: "300px", md: "400px" }, mt: "16px" ,  }} >Description</TextField>
+              <TextField  placeholder="Hint"sx={{ width: { xs: "300px", md: "400px" }, mt: "16px" ,  }} >Hint</TextField>
             </Box></item>
         </Grid>
-        <Grid item xs={12} md={6} >
-          <item> <Box bgcolor={"pink"} sx={{ height: "440px" }} >
-            {/* <TextField   sx={{ height: "340px", '& .MuiInputBase-root': { height: "100%" , overflow:"scroll" }, width: { xs:"100%",md:"360px"}, }}  multiline  ></TextField> */}
+        <Grid item xs={12} md={8} >
+          <item> 
+            <Box  
+            sx={{ 
+              bgcolor:"pink",
+              height: "440px",
+              margin: "auto",
+              display: "flex",
+              alignItems:"center",
+              justifyContent:"center" }} >
             <TextField
-        variant="outlined"
-        type="text"
-        multiline
-        maxRows={10}
-        placeholder="Type a message..."
-        style={{}}
-        
-        sx={{
-          flexGrow: 1,
-          fontSize: "clamp(14px, 2vw, 18px)",
-          bgcolor: "#00000026",
-          borderRadius: "20px",
-          "& .MuiOutlinedInput-root": {
-            padding: "8px 12px",
-            "& .MuiOutlinedInput-input": {
-              boxSizing: "border-box",
-              overflow:"hidden",
-            },
-            color: "white",
-            "&::placeholder": {
-              color: "white",
-            },
-          },
-          "& .MuiOutlinedInput-notchedOutline": {
-            border: "none",
-          },
-          "&:focus": {
-            outline: "none",
-          },
-        }}
-       
-      />
+              variant="outlined"
+              type="text"
+              multiline
+              maxRows={13}
+              placeholder="Answer"
+              style={{}}
+
+
+              sx={{
+                margin:'0 10px',
+                height: "340px",
+                width:  { xs: "260px", md: "320px" },
+                flexGrow: 1,
+                fontSize: "clamp(14px, 2vw, 18px)",
+                bgcolor: "#00000026",
+                borderRadius: "20px",
+                "& .MuiOutlinedInput-root": {
+                  padding: "8px 12px",
+                  "& .MuiOutlinedInput-input": {
+                    boxSizing: "border-box",
+                    overflow: "hidden",
+                  },
+                  color: "white",
+                  "&::placeholder": {
+                    color: "white",
+                  },
+                },
+                "& .MuiOutlinedInput-notchedOutline": {
+                  border: "none",
+                },
+                "&:focus": {
+                  outline: "none",
+                },
+              }}
+
+            />
           </Box></item>
         </Grid>
       </Grid>
+    </Box>
     </Box>
   );
 }
