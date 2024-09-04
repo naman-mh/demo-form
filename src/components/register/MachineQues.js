9559ee76ca0d116e021a28105fd84fc897deb1b5
Naman Malhotra <71208368+naman-mh@users.noreply.github.com>
2024-09-04 13:40:00 +0000

    some changes updated

--- src/components/register/MachineQues.js
+++ src/components/register/MachineQues.js
@@ -23,7 +23,40 @@ function MachineQues() {
         </Grid>
         <Grid item xs={12} md={6} >
           <item> <Box bgcolor={"pink"} sx={{ height: "440px" }} >
-            <TextField   sx={{ height: "340px", '& .MuiInputBase-root': { height: "100%" , overflow:"scroll" }, width: { xs:"100%",md:"360px"}, }}  multiline  ></TextField>
+            {/* <TextField   sx={{ height: "340px", '& .MuiInputBase-root': { height: "100%" , overflow:"scroll" }, width: { xs:"100%",md:"360px"}, }}  multiline  ></TextField> */}
+            <TextField
+        variant="outlined"
+        type="text"
+        multiline
+        maxRows={10}
+        placeholder="Type a message..."
+        style={{}}
+        
+        sx={{
+          flexGrow: 1,
+          fontSize: "clamp(14px, 2vw, 18px)",
+          bgcolor: "#00000026",
+          borderRadius: "20px",
+          "& .MuiOutlinedInput-root": {
+            padding: "8px 12px",
+            "& .MuiOutlinedInput-input": {
+              boxSizing: "border-box",
+              overflow:"hidden",
+            },
+            color: "white",
+            "&::placeholder": {
+              color: "white",
+            },
+          },
+          "& .MuiOutlinedInput-notchedOutline": {
+            border: "none",
+          },
+          "&:focus": {
+            outline: "none",
+          },
+        }}
+       
+      />
           </Box></item>
         </Grid>
       </Grid>
